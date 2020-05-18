1ce80623cc425623c81e100ec811dcb2009c3f1a
Dhanesh Pawar <dhaneshpawar.pro@gmail.com>
2020-05-18 20:57:32 +0530

    created first component

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <div className="header">
+        Hello i am Header at the CENTER
+    </div>
+  );
+}
+
+export default Header;
